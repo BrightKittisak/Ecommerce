795f7a000b4a7e69e6d471e4d2c495c6789c51f3
Kittisak <kittisak.yinmaroeng@gmail.com>
2025-02-25 09:16:56 +0700

    create-product-details-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,4 @@ export const APP_SLOGAN =
 export const APP_DESCRIPTION =
     process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
     'An e-commerce platform that makes shopping easy and worthwhile.'
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)

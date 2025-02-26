36bc39351d19be72798cceea139f5d8328de19e8
Kittisak <kittisak.yinmaroeng@gmail.com>
2025-02-26 09:36:00 +0700

    Signin-User

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,11 +1,15 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Ecommerce'
-export const APP_SLOGAN = 
+export const APP_SLOGAN =
     process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
 export const APP_DESCRIPTION =
     process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
     'An e-commerce platform that makes shopping easy and worthwhile.'
-export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
 
+export const APP_COPYRIGHT =
+    process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
+    `Copyright © 2025 ${APP_NAME}. All rights reserved.`
+    
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
 export const FREE_SHIPPING_MIN_PRICE = Number(
     process.env.FREE_SHIPPING_MIN_PRICE || 35
-  )
+)

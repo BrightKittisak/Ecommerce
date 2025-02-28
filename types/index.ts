b8d3c7d8e896876bdf2f95d58f9b5af90f01c3af
Kittisak <kittisak.yinmaroeng@gmail.com>
2025-02-28 10:05:21 +0700

    rate-review-Products

--- types/index.ts
+++ types/index.ts
@@ -1,20 +1,35 @@
-import { 
+import {
   CartSchema,
   OrderInputSchema,
-  OrderItemSchema, 
-  ProductInputSchema, 
-  ShippingAddressSchema, 
-  UserInputSchema, 
+  OrderItemSchema,
+  ProductInputSchema,
+  ShippingAddressSchema,
+  UserInputSchema,
   UserSignInSchema,
-  UserSignUpSchema, 
+  UserSignUpSchema,
+  ReviewInputSchema,
 } from '@/lib/validator'
 import { z } from 'zod'
+
+export type IReviewInput = z.infer<typeof ReviewInputSchema>
+export type IReviewDetails = IReviewInput & {
+  _id: string
+  createdAt: string
+  user: {
+    name: string
+  }
+}
 
 export type IProductInput = z.infer<typeof ProductInputSchema>
 
 export type Data = {
   users: IUserInput[]
   products: IProductInput[]
+  reviews: {
+    title: string
+    rating: number
+    comment: string
+  }[]
   headerMenus: {
     name: string
     href: string

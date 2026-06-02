4d10307e3a417b20254c2c83dfe4dd6f4c09cf77
Bright Kittisak <kittisak.yinmaroeng@gmail.com>
2026-06-02 20:21:35 +0700

    refactor(serialization): add typed client shape (#64)

--- lib/shared/serialization.ts
+++ lib/shared/serialization.ts
@@ -1,3 +1,37 @@
+type JsonPrimitive = string | number | boolean | null
+
+type JsonObject = {
+  [key: string]: JsonValue
+}
+
+type JsonValue = JsonPrimitive | JsonValue[] | JsonObject
+
+type HasToJSON = {
+  toJSON(): unknown
+}
+
+export type Serialized<T> =
+  T extends Date
+    ? string
+    : T extends HasToJSON
+      ? Serialized<ReturnType<T['toJSON']>>
+      : T extends JsonPrimitive
+        ? T
+        : T extends Array<infer Item>
+          ? Serialized<Item>[]
+          : T extends ReadonlyArray<infer Item>
+            ? Serialized<Item>[]
+            : T extends object
+              ? { [Key in keyof T]: Serialized<T[Key]> }
+              : T
+
+const serializeJson = (value: unknown): JsonValue =>
+  JSON.parse(JSON.stringify(value)) as JsonValue
+
+export function serializeTypedForClient<T>(value: T): Serialized<T> {
+  return serializeJson(value) as Serialized<T>
+}
+
 export function serializeForClient<T>(value: unknown): T {
   return JSON.parse(JSON.stringify(value)) as T
 }

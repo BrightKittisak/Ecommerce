43f7cddf0f7e9ae19436efd6e138630c87638091
bright <kittisak.yinmaroeng@gmail.com>
2026-06-02 14:45:35 +0700

    fix(user): normalize email at schema level

--- lib/db/models/user.model.ts
+++ lib/db/models/user.model.ts
@@ -16,7 +16,13 @@ export interface IUser extends Document, IUserInput {
 
 const userSchema = new Schema<IUser>(
   {
-    email: { type: String, required: true, unique: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
     name: { type: String, required: true },
     role: { type: String, required: true, default: 'User' },
     password: { type: String },

037be36f8067056fcb803c09d8d7c88781450ec8
bright <kittisak.yinmaroeng@gmail.com>
2026-06-06 18:15:01 +0700

    fix(cart): localize safe client errors

---
+++ lib/cart-client-copy.ts
@@ -0,0 +1,8 @@
+export const CART_ADD_SUCCESS_MESSAGE = 'เพิ่มลงตะกร้าแล้ว'
+export const CART_VIEW_CART_LABEL = 'ไปที่ตะกร้า'
+export const CART_ADD_BUTTON_LABEL = 'เพิ่มลงตะกร้า'
+export const CART_BUY_NOW_LABEL = 'ซื้อเลย'
+export const CART_CLIENT_ERROR_MESSAGE =
+  'ไม่สามารถอัปเดตตะกร้าได้ กรุณาลองใหม่อีกครั้ง'
+
+export const getCartClientErrorMessage = () => CART_CLIENT_ERROR_MESSAGE

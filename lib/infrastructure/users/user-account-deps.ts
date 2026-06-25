6b6050373670c60ec9e678aad1557baff1ec4b0b
bright <kittisak.yinmaroeng@gmail.com>
2026-06-25 14:57:09 +0700

    refactor(user): move db connection to account deps

--- lib/infrastructure/users/user-account-deps.ts
+++ lib/infrastructure/users/user-account-deps.ts
@@ -2,18 +2,21 @@ import bcrypt from 'bcryptjs'
 
 import type { RegisterUserAccountDeps } from '@/lib/application/users/register-user-account'
 import type { UpdateUserNameDeps } from '@/lib/application/users/update-user-name'
+import { connectToDatabase } from '@/lib/db'
 import User from '@/lib/db/models/user.model'
 
 const PASSWORD_HASH_SALT_ROUNDS = 12
 
 export const userAccountDeps: RegisterUserAccountDeps & UpdateUserNameDeps = {
-  createUserAccount(user) {
+  async createUserAccount(user) {
+    await connectToDatabase()
     return User.create(user)
   },
   hashPassword(password) {
     return bcrypt.hash(password, PASSWORD_HASH_SALT_ROUNDS)
   },
-  findUserById(userId) {
+  async findUserById(userId) {
+    await connectToDatabase()
     return User.findById(userId)
   },
 }
